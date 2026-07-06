287f67ed3ccb3b520d7d2e839af259e36b00df62
unknown <email-inkatriamanda@gmail.com>
2026-07-06 10:25:38 +0700

    refactor: ubah url gambar menjadi import lokal di folder constants

--- src/constants/exploreData.js
+++ src/constants/exploreData.js
@@ -1,12 +1,18 @@
 // src/constants/exploreData.jsx
 
+import istanaMaimunImg from "../assets/images/istanamaimun.png";
+import TjongAfie2Img from "../assets/images/Bangunan-bersejarah/tjong-afie-mansion-2.png";
+import KulinerImg from "../assets/images/kuliner-hero.png";
+import masjidRayaImg from "../assets/images/mesjid-raya.jpg";
+import JalanTolImg from "../assets/Teknologi-Modern/jalan-tol.png";
+
 export const exploreConfig = {
   items: [
     {
       id: 1,
       title: "Sejarah",
       desc: "Perjalanan panjang Kota Medan dari masa ke masa.",
-      imageUrl: "/images/explore-sejarah.jpg", // Pastikan gambar ini ada di folder public/images/
+      imageUrl: istanaMaimunImg, // Pastikan gambar ini ada di folder public/images/
       path: "/sejarah",
       //   icon: devIcon, // Gunakan ikon default atau ikon khusus jika ada
     },
@@ -14,7 +20,7 @@ export const exploreConfig = {
       id: 2,
       title: "Budaya & Tradisi",
       desc: "Keberagaman budaya yang hidup dan diwariskan.",
-      imageUrl: "/images/explore-budaya.jpg",
+      imageUrl: TjongAfie2Img,
       path: "/budaya",
       //   icon: devIcon,
     },
@@ -22,7 +28,7 @@ export const exploreConfig = {
       id: 3,
       title: "Kuliner Khas",
       desc: "Cita rasa khas Medan yang menggoda selera.",
-      imageUrl: "/images/explore-kuliner.jpg",
+      imageUrl: KulinerImg,
       path: "/kuliner",
       //   icon: devIcon,
     },
@@ -30,7 +36,7 @@ export const exploreConfig = {
       id: 4,
       title: "Wisata Unggulan",
       desc: "Tempat wisata terbaik yang wajib dikunjungi.",
-      imageUrl: "/images/explore-wisata.jpg",
+      imageUrl: masjidRayaImg,
       path: "/wisata",
       //   icon: devIcon,
     },
@@ -38,7 +44,7 @@ export const exploreConfig = {
       id: 5,
       title: "Potensi Modern",
       desc: "Inovasi dan pembangunan untuk masa depan Medan.",
-      imageUrl: "/images/explore-modern.jpg",
+      imageUrl: JalanTolImg,
       path: "/tech",
       //   icon: devIcon,
     },
